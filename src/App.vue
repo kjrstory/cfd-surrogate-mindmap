a337927488006f9b6767d2f93d232fe09227e179
kjrstory <kjrstory@gmail.com>
2025-05-09 14:48:57 +0000

    App vue update

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,20 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="app">
+    <!-- MindMap 컴포넌트에 JSON 트리 데이터 전달 -->
+    <MindMap :tree="treeData" />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+<script lang="ts" setup>
+// JSON 파일 import
+import treeData from '/assets/data.json'
+// 방금 만든 MindMap.vue import
+import MindMap from './components/MindMap.vue'
+</script>
+
+<style>
+/* 필요하면 전역 스타일 추가 */
+#app {
+  padding: 1rem;
 }
 </style>
